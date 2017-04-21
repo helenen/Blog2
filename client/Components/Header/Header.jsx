e7ad4f012b95c7cd3f42fd4f09c60904d7e496d7
Helene Niassy <heleneniassy@MacBook-Pro-de-Helene.local>
2017-04-21 17:21:23 +0200

    add photo

--- client/Components/Header/Header.jsx
+++ client/Components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import LazyLoad from 'react-lazy-load';
+import pageAccueil from "../../img/page-accueil.jpg";
 
  class Header extends React.Component {
   render() {
     return (
      <div>
-        <h1>Hello World</h1>
-      </div>
+       guhuhihjpi
+       <img src={pageAccueil} />
+     </div>
     );
   }
 }
