eca5e0b34bb9d5da9e8b56c6589080bb169b7458
Helene Niassy <heleneniassy@MacBook-Pro-de-Helene.local>
2017-05-31 23:25:22 +0200

    add screen projects

--- client/Components/Projects/Projects.jsx
+++ client/Components/Projects/Projects.jsx
@@ -3,73 +3,40 @@ import Menu from '../Menu/Menu.jsx';
 import Grid from '../Grid/Grid.scss';
 import './Projects.scss';
 import Assets from "../../Assets.js";
-import axios from 'axios';
+import photoOne from "../Assets/photo-one.jpg";
+import photoTwo from "../Assets/photo-two.jpg";
+import photoThree from "../Assets/photo-three.jpg";
+
 
 class Projects extends React.Component {
-  constructor() {
-      super();
-      this.state = {
-        gifs: []
-      };
-    }
-
-    componentDidMount() {
-      axios.get('https://api.unsplash.com/collections/featured')
-        .then(response => {
-          this.setState({
-            gifs: response.data.pokemon
-          });
-        })
-        .catch(error => {
-          console.log('Error fetching and parsing data', error);
-        });
-    }
   render() {
-    console.log(this.state.gifs);
     return (
       <div>
         <Menu className="menu-projects" />
           <div className="grid-container-main">
-            <div className="grid-container">
+            <div className="grid-container-project">
               <div className="row">
-                <div className="grid">
-                    <img src='https://source.unsplash.com/collection/617562/320x300'  />
+                <div className="grid-project">
+                    <img src={photoOne} />
                 </div>
-                <div className="grid">
-                  <img src='https://source.unsplash.com/collection/617562/320x300'/>
-                </div>
-                <div className="grid">
-                  <img src='https://source.unsplash.com/collection/617562/320x300' />
+                <div >
+                  <a href="https://tintsofpink.herokuapp.com/">
+                    <img src={photoTwo} className="grid-project"/>
+                  </a>
                 </div>
               </div>
               <div className="row">
-                <div className="grid">
-                    <img src="https://unsplash.it/320/300?random=4" />
-                </div>
-                <div className="grid">
-                  <img src='https://unsplash.it/320/300?random=5' />
-                </div>
-                <div className="grid">
-                  <img src='https://unsplash.it/320/300?random=6' />
-                </div>
-              </div>
-              <div className="row">
-                <div className="grid">
-                    <img src="https://unsplash.it/320/300?random=7" />
-                </div>
-                <div className="grid">
-                  <img src='https://unsplash.it/320/300?random=8' />
-                </div>
-                <div className="grid">
-                  <img src='https://unsplash.it/320/300?random=9' />
+                <div >
+                  <a href="https://helenen.github.io/carnet-de-voyage/">
+                    <img src={photoThree} className="grid-project"/>
+                    </a>
                 </div>
               </div>
             </div>
           </div>
-      </div>
-    );
+        </div>
+      );
+    }
   }
-
-}
 
 export default Projects;
